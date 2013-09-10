469c550dd85d0df60aec33e5a921b671e3728c7e
Nicholas Kwan <ready725@gmail.com>
2013-09-10 18:59:16 +0800

    Move files Add: Support for PUT/DELETE

--- src/todoapp/js/manager.js
+++ src/todoapp/js/manager.js
@@ -13,10 +13,16 @@ try {
 var idgen = 0;
 function addEntry(target) {
 	var newEntry = document.getElementById(target);
-	entries[Date.now() + "_" + idgen] = newEntry.value;
+	var entry = {
+		id: Date.now() + "_" + idgen,
+		value: newEntry.value
+	}
+	entries[entry.id] = entry.value;
 	newEntry.value = "";
 	showEntries();
 	syncLocal();
+	
+	serverPut(entry);
 }
 
 function submit(e, callback, argument) {
@@ -42,4 +48,39 @@ function showEntries() {
 		result += "<div class=\"entry\">" + entries[entry] + " " + "<a href=\"javascript:deleteEntry(\'" + entry + "\');\">(x)</a>" + "</div>";
 	}
 	$("#listings").get(0).innerHTML = result;
+}
+
+var user = "demo";
+
+function serverGet() {
+	$.ajax({
+		url: 'api.php',
+		type: 'GET',
+		data: "user="+user,
+		success: function(result) {
+			console.log(result);
+		}
+	});
+}
+function serverPut(entry) {
+	var entryStr = JSON.stringify(entry);
+	$.ajax({
+		url: 'api.php',
+		type: 'PUT',
+		data: "user=" + user + "&entry="+entryStr,
+		success: function(result) {
+			console.log(result);
+		}
+	});
+}
+function serverDelete(id) {
+	var entryStr = JSON.stringify(entry);
+	$.ajax({
+		url: 'api.php',
+		type: 'DELETE',
+		data: "id="+id,
+		success: function(result) {
+			console.log(result);
+		}
+	});
 }
