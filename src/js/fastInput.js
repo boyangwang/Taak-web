4a8c0fc03e593da825e221d43892e8a95a913379
cl <cl@startup.org.sg>
2013-09-14 23:19:42 +0800

    minor

--- src/js/fastInput.js
+++ src/js/fastInput.js
@@ -16,6 +16,10 @@ var fastInputs = {
 	],
 };//Endof fastInputs
 
+var userPersonalDict = {
+	actions: [],
+	objects: [],
+};//Endof userPersonalDict
 
 
 function searchFastInputs(query, process){
@@ -24,9 +28,7 @@ function searchFastInputs(query, process){
 
 
 $(function(){
-
-
-	//should be #inputField. but wont change name in case break other stuff.
+	//should be #inputField. but wont change name... in case break other stuff.
 	$("#entryBox.typeahead").typeahead({
 		local: fastInputs_ver1,
 		// source: function(query, process){ /// https://www.google.com.sg/search?q=typeahead+dynamic+source&start=10	/// http://fusiongrokker.com/post/heavily-customizing-a-bootstrap-typeahead
@@ -36,12 +38,12 @@ $(function(){
 		// items: 7,
 		// //window.fastInputs
 	});
-	//Temporary Hack Fix for typeahead screwing up existing CSS -> it creates some span and newly encloses your input field.
-	///https://www.google.com.sg/search?q=twitter+typeahead+control+width
-	$(".twitter-typeahead").css('width', "100%"); // cannot $(".typeahead").width() + "px" because the typeahead input is using 100% inherit from parent, and this DOM is the parent.
-	$('.tt-hint').css('width', "99.5%"); //needs to be 99.5% or else will have some crap appearing on the right side.
-	$('.tt-dropdown-menu').css('width', "100%");
 
+	//AAAHHHH. Temporary Hack Fix for typeahead screwing up existing CSS -> it creates some span and newly encloses your input field.
+	///https://www.google.com.sg/search?q=twitter+typeahead+control+width	/// https://github.com/twitter/typeahead.js/issues/276
+	$(".twitter-typeahead").css('width', "100%"); // cannot $(".typeahead").width() + "px" because the typeahead input is using 100% inherit from parent, and this DOM is the parent.
+	$('.tt-hint').css('width', "99.5%"); //needs to be 99.5%. cannot 99% or 100%. or else will have some crap appearing on the right side.
+	$('.tt-dropdown-menu').css('width', "100%");
 });
 
 var fastInputs_ver1 = [
