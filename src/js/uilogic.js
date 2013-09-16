86311e40bfe4794b856d1729700839cebf30c4bf
noobcake123 <benjyyap@gmail.com>
2013-09-17 05:16:37 +0800

    Integrating UI

---
+++ src/js/uilogic.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	console.log("ready");
+	$("#addTaskIcon").click(function(){
+		addTask();
+	});
+});
+
+function addTask(){
+	var task = $(document.createElement('div')).attr('class','task');
+	task.draggable().resizable({
+		minHeight:80,minWidth:80
+	}
+		);
+	var taskText = $(document.createElement('div')).attr('class','taskText');
+	taskText.attr('contenteditable','true');
+	task.append(taskText);
+	$(".workflowView").append(task);
+	$(task.get(0)).children().each(function(){
+		if($(this).attr('class') == "taskText"){
+			$(this).focus();
+		}
+	});
+}
