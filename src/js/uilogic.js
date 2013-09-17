666071fec7c5c2f87a4bd77917ec00444c512f33
noobcake123 <benjyyap@gmail.com>
2013-09-18 07:44:30 +0800

    Added temporary (ugly) instructions in UI in case not intuitive enough. Will probably change it.

--- src/js/uilogic.js
+++ src/js/uilogic.js
@@ -4,67 +4,87 @@ $(document).ready(function(){
 	$("#addTaskIcon").mousedown(function(){
 		addTask();
 	});
-	*/
-	$("#addTaskIconYellow").draggable({
-		stop:function(){
-			if($(this).offset().left > 100){
-				var taskPositionX = $(this).offset().left+20;
-				var taskPositionY = $(this).offset().top;
-				$(this).css({
-					"left":"50%",
-					"top":"50px"
-				});
-				addTask(taskPositionX,taskPositionY,"yellow");
-			}
-			else{
-				$(this).css({
-					"left":"50%",
-					"top":"50px"
-				});
-			}
+*/
+$("#addTaskIconYellow").mousedown(function(){
+	$("#dragInstructions").show();
+}).mouseup(function(){
+	$("#dragInstructions").hide();
+});
+$("#addTaskIconRed").mousedown(function(){
+	$("#dragInstructions").show();
+}).mouseup(function(){
+	$("#dragInstructions").hide();
+});
+$("#addTaskIconBlue").mousedown(function(){
+	$("#dragInstructions").show();
+}).mouseup(function(){
+	$("#dragInstructions").hide();
+});
+
+$("#addTaskIconYellow").draggable({
+	stop:function(){
+		$("#dragInstructions").hide();
+		if($(this).offset().left > 100){
+			var taskPositionX = $(this).offset().left+20;
+			var taskPositionY = $(this).offset().top;
+			$(this).css({
+				"left":"50%",
+				"top":"50px"
+			});
+			addTask(taskPositionX,taskPositionY,"yellow");
 		}
-	});
-	$("#addTaskIconRed").draggable({
-		stop:function(){
-			if($(this).offset().left > 100){
-				var taskPositionX = $(this).offset().left+20;
-				var taskPositionY = $(this).offset().top;
-				$(this).css({
-					"left":"50%",
-					"top":"150px"
-				});
-				addTask(taskPositionX,taskPositionY,"red");
-			}
-			else{
-				$(this).css({
-					"left":"50%",
-					"top":"150px"
-				});
-			}
+		else{
+			$(this).css({
+				"left":"50%",
+				"top":"50px"
+			});
 		}
-	});
-	$("#addTaskIconBlue").draggable({
-		stop:function(){
-			if($(this).offset().left > 100){
-				var taskPositionX = $(this).offset().left+20;
-				var taskPositionY = $(this).offset().top;
-				$(this).css({
-					"left":"50%",
-					"top":"250px"
-				});
-				addTask(taskPositionX,taskPositionY,"blue");
-			}
-			else{
-				$(this).css({
-					"left":"50%",
-					"top":"250px"
-				});
-			}
+	}
+});
+$("#addTaskIconRed").draggable({
+	stop:function(){
+		$("#dragInstructions").hide();
+		if($(this).offset().left > 100){
+			var taskPositionX = $(this).offset().left+20;
+			var taskPositionY = $(this).offset().top;
+			$(this).css({
+				"left":"50%",
+				"top":"150px"
+			});
+			addTask(taskPositionX,taskPositionY,"red");
 		}
-	});
+		else{
+			$(this).css({
+				"left":"50%",
+				"top":"150px"
+			});
+		}
+	}
+});
+$("#addTaskIconBlue").draggable({
+	stop:function(){
+		$("#dragInstructions").hide();
+		if($(this).offset().left > 100){
+			var taskPositionX = $(this).offset().left+20;
+			var taskPositionY = $(this).offset().top;
+			$(this).css({
+				"left":"50%",
+				"top":"250px"
+			});
+			addTask(taskPositionX,taskPositionY,"blue");
+		}
+		else{
+			$(this).css({
+				"left":"50%",
+				"top":"250px"
+			});
+		}
+	}
+});
 	UI_init(); // located in client.js
 });
 
 function addTask(x,y,taskColor){
+	$("#dragInstructions").hide();
 	UI_addTaskPanel(null,x,y,taskColor);
 }
