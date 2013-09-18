4e3abbc1d6b7462cfd628c014f03116bdbc3ed9f
noobcake123 <benjyyap@gmail.com>
2013-09-19 03:10:16 +0800

    UI now supports more colour tasks and minor UI tweaks

--- src/js/uilogic.js
+++ src/js/uilogic.js
@@ -1,97 +1,25 @@
 $(document).ready(function(){
 	console.log("ready");
-	/*
-	$("#addTaskIcon").mousedown(function(){
-		addTask();
+
+	// @boyang
+	$('#loginPrompt').click(hideLoginPrompt);
+
+	$(".addTaskDiv").draggable({
+		revert:function(){
+			console.log("offset: " + $(this).offset().left);
+			if($(this).offset().left > 220){
+				var taskPositionX = $(this).offset().left+20;
+				var taskPositionY = $(this).offset().top-30;
+				var taskColor = $(this).attr('data');
+				console.log(taskColor);
+				addTask(taskPositionX,taskPositionY,taskColor);
+			}
+			return true;
+		},
+		revertDuration:0
 	});
-*/
-
-// @boyang
-$('#loginPrompt').click(hideLoginPrompt);
-
-$("#addTaskIconYellow").mousedown(function(){
-	showDragInstructions();
-}).mouseup(function(){
-	hideDragInstructions();
-});
-$("#addTaskIconRed").mousedown(function(){
-	showDragInstructions();
-}).mouseup(function(){
-	hideDragInstructions();
-});
-$("#addTaskIconBlue").mousedown(function(){
-	showDragInstructions();
-}).mouseup(function(){
-	hideDragInstructions();
-});
-
-
-$("#addTaskIconYellow").draggable({
-	stop:function(){
-		hideDragInstructions();
-		if($(this).offset().left > 100){
-			var taskPositionX = $(this).offset().left+20;
-			var taskPositionY = $(this).offset().top;
-			$(this).css({
-				"left":"50%",
-				"top":"50px"
-			});
-			addTask(taskPositionX,taskPositionY,"yellow");
-		}
-		else{
-			$(this).css({
-				"left":"50%",
-				"top":"50px"
-			});
-		}
-	}
-});
-$("#addTaskIconRed").draggable({
-	stop:function(){
-		hideDragInstructions();
-		if($(this).offset().left > 100){
-			var taskPositionX = $(this).offset().left+20;
-			var taskPositionY = $(this).offset().top;
-			$(this).css({
-				"left":"50%",
-				"top":"150px"
-			});
-			addTask(taskPositionX,taskPositionY,"red");
-		}
-		else{
-			$(this).css({
-				"left":"50%",
-				"top":"150px"
-			});
-		}
-	}
-});
-$("#addTaskIconBlue").draggable({
-	stop:function(){
-		hideDragInstructions();
-		if($(this).offset().left > 100){
-			var taskPositionX = $(this).offset().left+20;
-			var taskPositionY = $(this).offset().top;
-			$(this).css({
-				"left":"50%",
-				"top":"250px"
-			});
-			addTask(taskPositionX,taskPositionY,"blue");
-		}
-		else{
-			$(this).css({
-				"left":"50%",
-				"top":"250px"
-			});
-		}
-	}
-});
 	UI_init(); // located in client.js
 });
-
-function hideLoginPrompt() {
-	$('#loginPrompt').fadeOut(300);
-}
 
 function showDragInstructions() {
 	$("#dragInstructions").fadeIn(300);
@@ -103,4 +31,8 @@ function hideDragInstructions() {
 function addTask(x,y,taskColor){
 	hideDragInstructions(); // already called earlier
 	UI_addTaskPanel(null,x,y,taskColor);
+}
+
+function hideLoginPrompt() {
+	$('#loginPrompt').fadeOut(300);
 }
