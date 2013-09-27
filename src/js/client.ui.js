e21eec30c6e1c86f70a8f181f2a1b9a3c6433020
Nicholas Kwan <ready725@gmail.com>
2013-09-27 12:19:07 +0800

    Removed bug report (fixed in 6a36840e6f12db8e87509adae052b9257157fc7c)

--- src/js/client.ui.js
+++ src/js/client.ui.js
@@ -626,10 +626,6 @@ function UI_addTaskPanel(entry,baseOffsetX,baseOffsetY,taskColor) {
 		lastTask = task; // lastTask represents the current task being edited
 		selectedTask = task;
 		UI_showColorSwitcher(task);
-		///### BUG: Drag drop. Click color on ColorSwitcher. Drag drop.
-		///### Click to edit text. With empty textbox and press enter.
-		///### Now you get infinite recursive new notes each time you press enter.
-		///### Repeat drag drop click colorswitcher for more notes appearing with each enter.
 		e.stopPropagation(); // don't send click event to parent
 	});
 
